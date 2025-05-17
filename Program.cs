e431886138866bc795e7684cec919aa80a944ea5
Ironweed <s.chuprakov@gmail.com>
2025-05-17 14:20:08 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace if_else_for
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
