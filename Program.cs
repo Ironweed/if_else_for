bbf451e73624086d5d9f5e594fe5fde5b98c942a
Ironweed <s.chuprakov@gmail.com>
2025-05-18 14:07:47 +0300

    for

--- Program.cs
+++ Program.cs
@@ -8,6 +8,44 @@ namespace if_else_for
         {
 
 
+            for (int i = 5; i > 0; i--)
+            {
+                Console.WriteLine("Iteration {0}", i);
+                switch (Console.ReadLine())
+                {
+                    case "red":
+                        Console.BackgroundColor = ConsoleColor.Red;
+                        Console.ForegroundColor = ConsoleColor.Black;
+
+                        Console.WriteLine("Your color is red!");
+                        break;
+
+                    case "green":
+                        Console.BackgroundColor = ConsoleColor.Green;
+                        Console.ForegroundColor = ConsoleColor.Black;
+
+                        Console.WriteLine("Your color is green!");
+                        break;
+
+                    case "cyan":
+                        Console.BackgroundColor = ConsoleColor.Cyan;
+                        Console.ForegroundColor = ConsoleColor.Black;
+
+                        Console.WriteLine("Your color is cyan!");
+                        break;
+                    default:
+                        Console.BackgroundColor = ConsoleColor.Yellow;
+                        Console.ForegroundColor = ConsoleColor.Black;
+
+                        Console.WriteLine("Your color is yellow!");
+                        break;
+                }
+            }
+
+
+/*
+ 
+            //////////////////////////////////////////////////////////////////////////
             Console.WriteLine("Напишите свой любимый цвет на английском с маленькой буквы");
 
 		var color = Console.ReadLine();
@@ -44,7 +82,7 @@ namespace if_else_for
 				break;
 		}
 
-/*
+
             //////////////////////////////////////////////////////////////////////////
             Console.WriteLine("Напишите свой любимый цвет на английском с маленькой буквы");
 
